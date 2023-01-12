9fe442cb9ec7d043056d62ae3b751ac6d7455b27
joemeetjoe <joemeetjoe@gmail.com>
2023-01-12 14:00:25 -0500

    refactored functions to make it more readable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,48 @@
-//going to use this for search bar
-function getIndividualStats () {
-    var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/ditto`
-    
+// main fetch function that gets the name of all 151 pokemon only
+function getAll151FetchCall(limit){
+    var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}`
+    // fetch call
     fetch(pokeApiCallTemplate)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
-            // console.log(data);
-        //    let pokemonData = data;
-        
-            
-        })
-        } 
-//add this to an event listener
-getIndividualStats()
-// main fetch function
-function fetchCall(){
-    var pokeApiCallTemplate = "https://pokeapi.co/api/v2/pokemon/?limit=151"
-    
-    fetch(pokeApiCallTemplate)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            
-        //    let pokemonData = data;
-        consoleData(data)
+        // plug in to display pokemon names with the data
         displayPokemonNames(data);
-        //  showPokemonInfo(data)   
         })
-        } 
+        };
 
-
-
-
-
-fetchCall();
-function consoleData(stuff) {
-    console.log(stuff)
-}
-
-
+// loops through all the 151 names given by getAll151FetchCall and iterates through them, plugging them 
+// into the showPokemon info function
 function displayPokemonNames (data){
     PokemonDataArray = data.results;
     for(i = 0; i < PokemonDataArray.length; i ++){
         PokemonNames = data.results[i].name;
         showPokemonInfo(PokemonNames);
+    };
+};
 
-    }
-   
-}
-
-function showPokemonInfo (p) {
-    
-        var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/${p}`
-    
+// function to plug in info and call for pokemon based off of names
+// plug in the pokemon name to get all the data for that pokemon
+function showPokemonInfo (pokemonName) {
+    // template that will plug in the parameter pokemon name
+    var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    // fetch call 
     fetch(pokeApiCallTemplate)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
             console.log(data);
-        //    let pokemonData = data;
-        
-            
         })
-        } 
+        };
+
+
+
+getAll151FetchCall(20);
+
+
+
+
+
     

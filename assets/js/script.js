1000986afcb4ed84a184e5e66068c21eef8bba14
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-01-13 19:49:38 -0500

    made the grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,125 @@
 // main fetch function that gets the name of all 151 pokemon only
-let offset = 0
-function getAll151FetchCall(limit){
-    var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`
-    // fetch call
-    fetch(pokeApiCallTemplate)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-        // plug in to display pokemon names with the data
-        displayPokemonNames(data);
-        // console.log(data);
-        })
-        };
+let offset = 0;
+function getAll151FetchCall(limit) {
+  var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`;
+  // fetch call
+  fetch(pokeApiCallTemplate)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // plug in to display pokemon names with the data
+      displayPokemonNames(data);
+      // console.log(data);
+    });
+}
 
-        // loops through all the 151 names given by getAll151FetchCall and iterates through them, plugging them 
+// loops through all the 151 names given by getAll151FetchCall and iterates through them, plugging them
 // into the showPokemon info function
-function displayPokemonNames (data){
-    pokemonDataArray = data.results;
-    syncedUpDataArray = [];
-    // console.log(PokemonDataArray);
-    for( let i = 0; i < pokemonDataArray.length; i++){
-        PokemonNames = data.results[i].name;
-        // console.log(PokemonNames);
-        syncedUpDataArray.push(PokemonNames);
-        // console.log(syncedUpDataArray);
-       
-    };
-    showPokemonInfo(syncedUpDataArray);
-};
+function displayPokemonNames(data) {
+  pokemonDataArray = data.results;
+  syncedUpDataArray = [];
+  // console.log(PokemonDataArray);
+  for (let i = 0; i < pokemonDataArray.length; i++) {
+    PokemonNames = data.results[i].name;
+    // console.log(PokemonNames);
+    syncedUpDataArray.push(PokemonNames);
+    // console.log(syncedUpDataArray);
+  }
+  showPokemonInfo(syncedUpDataArray);
+}
 
 // function to plug in info and call for pokemon based off of names
 // plug in the pokemon name to get all the data for that pokemon
-function showPokemonInfo (pokemonName) {
-    // console.log(pokemonName);
-    // template that will plug in the parameter pokemon name
-    for( var i = 0; i < pokemonName.length; i++){
-        // console.log(pokemonName[i]);
-        var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/${pokemonName[i]}`
-    // fetch call 
-        fetch(pokeApiCallTemplate)
-            .then(function (response){
-                return response.json();
-            })
-            .then(function (data){
-                // console.log(data);
-                
-                let pokemonArray = []
-                pokemonArray.push(data)
-                // console.log(pokemonArray)
-                displayList(pokemonArray);
-                return(pokemonArray);
-            });
-    };
-        
-};
- 
+function showPokemonInfo(pokemonName) {
+  // console.log(pokemonName);
+  // template that will plug in the parameter pokemon name
+  for (var i = 0; i < pokemonName.length; i++) {
+    // console.log(pokemonName[i]);
+    var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/${pokemonName[i]}`;
+    // fetch call
+    fetch(pokeApiCallTemplate)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // console.log(data);
+
+        let pokemonArray = [];
+        pokemonArray.push(data);
+        // console.log(pokemonArray)
+        displayList(pokemonArray);
+        return pokemonArray;
+      });
+  }
+}
+
 // display the list of pokemon that was fetched along with their stats
-function displayList (items) {
-    let listOutput = document.querySelector('.list-output')
-    console.log(items);
-    if (items.length == 0){
-        listOutput.innerHTML = " "
-    } else {
-    
+function displayList(items) {
+  let listOutput = document.querySelector(".list-output");
+  console.log(items);
+  if (items.length == 0) {
+    listOutput.innerHTML = " ";
+  } else {
     card = `
-     <div class="wrapper">
+     <div class="wrapper card">
          
          <div class="info-wrapper">
              <h1>${items[0].name}</h1>
              <div class="stats-wrapper">
-             <img class="w-25" src=${items[0].sprites.front_default} alt=${items[0].name}/>
-                 <div class="hp-div">HP: ${items[0].stats[0].base_stat}</div>
-                 <div class="attack-div">Attack: ${items[0].stats[1].base_stat}</div>
-                 <div class="defense-div">Defense: ${items[0].stats[2].base_stat}</div>
-                 <div class="specialatk-div">Special-Attack: ${items[0].stats[3].base_stat}</div>
-                 <div class="specialdef-div">Special-Defense: ${items[0].stats[4].base_stat}</div>
-                 <div class="speed-div">speed: ${items[0].stats[5].base_stat}</div>
+             <img class="w-100" src=${items[0].sprites.front_default} alt=${items[0].name}/>
+                 <a href="#" class="add-btn btn btn-primary">Add</a>
              </div>
          </div>
      </div>
-     `
-    listOutput.innerHTML += card
- };
-    };
-    
-     
+     `;
+    listOutput.innerHTML += card;
+  }
+}
+
+function showStats() {}
 
 // setting up the query selector for the search button, when clicked, exectute the function searchPokemon;
-const searchBtn = document.querySelector('#search-btn');
- searchBtn.addEventListener('click', searchPokemon);
+const searchBtn = document.querySelector("#search-btn");
+searchBtn.addEventListener("click", searchPokemon);
 //setting up the search pokemon function, preventing default, getting the value from form control,
 // and calling for the show single pokemon info function
-function searchPokemon (event) {
-    event.preventDefault()
-    const searchValue = document.querySelector('.form-control').value
-    showSinglePokemonInfo(searchValue) 
-    // console.log(searchValue)
+function searchPokemon(event) {
+  event.preventDefault();
+  const searchValue = document.querySelector(".form-control").value;
+  showSinglePokemonInfo(searchValue);
+  // console.log(searchValue)
 }
 // setting up the show single pokemon info
-function showSinglePokemonInfo (pokemonName) {
-    // template that will plug in the parameter pokemon name
-    var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    // fetch call 
-    fetch(pokeApiCallTemplate)
-        .then(function (response){
-            if (response.status == 404) {
-                document.querySelector("#fetched-pokemon").innerHTML = "pokemon not found"; 
-            } else {
-                return response.json();
-            }
-        })
-        .then(function (data){
-            // console.log(data);
-            // if statement making sure that the pokemon searched for is within the original 151
-             if (data.id < 152) {
-                displaySearchedPokemon(data);
-            } else {
-                document.querySelector("#fetched-pokemon").innerHTML = "pokemon not found";
-            }
-        });
-        };
-//function setting up the display searched pokemon 
-function displaySearchedPokemon (info) {
-    // console.log(info)
-    const pokemonOutput = document.querySelector("#fetched-pokemon");
-    const card = `
+function showSinglePokemonInfo(pokemonName) {
+  // template that will plug in the parameter pokemon name
+  var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  // fetch call
+  fetch(pokeApiCallTemplate)
+    .then(function (response) {
+      if (response.status == 404) {
+        document.querySelector("#fetched-pokemon").innerHTML =
+          "pokemon not found";
+      } else {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      // console.log(data);
+      // if statement making sure that the pokemon searched for is within the original 151
+      if (data.id < 152) {
+        displaySearchedPokemon(data);
+      } else {
+        document.querySelector("#fetched-pokemon").innerHTML =
+          "pokemon not found";
+      }
+    });
+}
+//function setting up the display searched pokemon
+function displaySearchedPokemon(info) {
+  // console.log(info)
+  const pokemonOutput = document.querySelector("#fetched-pokemon");
+  const card = `
     <div class="wrapper">
         <img class="w-100" src=${info.sprites.front_default} alt=${info.name}/>
         <div class="info-wrapper">
@@ -140,42 +134,43 @@ function displaySearchedPokemon (info) {
             </div>
         </div>
     </div>
-    `
+    `;
 
-    pokemonOutput.innerHTML = card;
-};
+  pokemonOutput.innerHTML = card;
+}
 
 // pulling the values for the left and right buttons
 var leftArrow = document.getElementById("left-arrow");
 var rightArrow = document.getElementById("right-arrow");
 // adding the function for leftPagination
-function rightPagination(){
-    offset += 20; 
-    var emptyDisplayList = [];
-    displayList(emptyDisplayList);
-    getAll151FetchCall(20);
-    
-    
+function rightPagination() {
+  offset += 20;
+  var emptyDisplayList = [];
+  displayList(emptyDisplayList);
+  getAll151FetchCall(20);
 }
 
-function leftPagination(){
-    if (offset == 0 || offset == 131 ){
-        return;
-    } else {
+function leftPagination() {
+  if (offset == 0 || offset == 131) {
+    return;
+  } else {
     offset -= 20;
     var emptyDisplayList = [];
     displayList(emptyDisplayList);
     getAll151FetchCall(20);
     console.log(offset);
-    };
-};
+  }
+}
 // adding event listeners to the buttons
 leftArrow.addEventListener("click", leftPagination);
 rightArrow.addEventListener("click", rightPagination);
 
+// on starting of the page, place in 20 pokemon to the call.
+getAll151FetchCall(20);
 
-
-
-
- // on starting of the page, place in 20 pokemon to the call. 
- getAll151FetchCall(20);
+//  <div class="hp-div">HP: ${items[0].stats[0].base_stat}</div>
+//                  <div class="attack-div">Attack: ${items[0].stats[1].base_stat}</div>
+//                  <div class="defense-div">Defense: ${items[0].stats[2].base_stat}</div>
+//                  <div class="specialatk-div">Special-Attack: ${items[0].stats[3].base_stat}</div>
+//                  <div class="specialdef-div">Special-Defense: ${items[0].stats[4].base_stat}</div>
+//                  <div class="speed-div">speed: ${items[0].stats[5].base_stat}</div>

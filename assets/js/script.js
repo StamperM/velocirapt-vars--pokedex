21cdaea62fd10de20c4cb4d2f14bc9b74c76ea52
joemeetjoe <joemeetjoe@gmail.com>
2023-01-14 19:44:58 -0500

    refactored code, fixed bug with pagination button, added png files, added some styling with cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // main fetch function that gets the name of all 151 pokemon only
-
-let offset = 0
+let offset = 0;
 function getAll151FetchCall(limit){
     var pokeApiCallTemplate = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`
     // fetch call
@@ -14,10 +13,8 @@ function getAll151FetchCall(limit){
         })
         };
 
-
 // loops through all the 151 names given by getAll151FetchCall and iterates through them, plugging them
 // into the showPokemon info function
-
 function displayPokemonNames (data){
     pokemonDataArray = data.results;
     syncedUpDataArray = [];
@@ -29,7 +26,6 @@ function displayPokemonNames (data){
         // console.log(syncedUpDataArray);
        
     };
-    console.log(syncedUpDataArray)
     showPokemonInfo(syncedUpDataArray);
 };
 
@@ -47,79 +43,52 @@ function showPokemonInfo (pokemonName) {
             return response.json();
         })
         .then(function (data){
-            console.log(data);
-            
             let pokemonArray = []
             pokemonArray.push(data)
-            console.log(pokemonArray)
             displayList(pokemonArray)
-           
-            useFilter(pokemonArray)
-           
-            
-            
-            
-            return(pokemonArray);
-           
-             
-        })
-        };
-    }
-
-
-
-
- 
+           useFilter(pokemonArray)
+          })
+    };
+  };
 
 // display the list of pokemon that was fetched along with their stats
-
-function displayList (items) {
+function displayList (items){
     let listOutput = document.querySelector('.list-output')
-    console.log(items);
+    
     if (items.length == 0){
         listOutput.innerHTML = " "
-    } else {
-    
-    const card = `
-     <div id=${items[0].types[0].type.name} class="wrapper list-wrapper">
-
-         
-         <div class="info-wrapper">
-             <h1>${items[0].name}</h1>
-             <div class="stats-wrapper">
-             <img class="w-100" src=${items[0].sprites.front_default} alt=${items[0].name}/>
-                 <a href="#" class="add-btn btn btn-primary">Add</a>
-             </div>
-         </div>
+    } else if (items[0].id > 151) {
+       var card = ` `;
+    } else{ card =
+      ` 
+      <div class="wrapper list-wrapper h25 ${items[0].types[0].type.name}">
+      <div class="info-wrapper">
+             <h1 id="pokemon-name">${items[0].name} </h1>
+             <div class="stats-wrapper h25">
+                <img id= "pokemon-img" class="w-75" src=${"./assets/pokemon/" + items[0].id + ".png"} alt=${items[0].name}/>
+                <a href="#" class="add-btn btn btn-primary">Add</a>
+              </div>
+          </div>
      </div>
-
-     `
+      `
     listOutput.innerHTML += card 
-    
- };
     };
+  };
     
-     
-
-
-
+//function to use filter when the dropdown menu is pulled down and something is selected.  
 function useFilter (info) {
-const dropDown = document.querySelectorAll('.dropdown-item')
+const dropDown = document.querySelectorAll('.dropdown-item');
 
 dropDown.forEach((each, key) => {
-    const value = each.getAttribute('id')
-    const types = info[0].types[0].type.name
-    console.log(value + "=" + types)
-              
+    const value = each.getAttribute('id');
+    const types = info[0].types[0].type.name;
     each.addEventListener("click", () => {
-        
-        filteredData(value,types,info)
-            
-        })
-        
-        
-    })
-}
+        filteredData(value,types,info);
+      });
+  });
+};
+
+//setting up the filtered data function that will filter through the types of pokemon, ex. fire, grass, rock...
 function filteredData (value, types, info) {
     let filteredArray = []
     const wrapper = document.querySelectorAll('.list-wrapper')
@@ -133,25 +102,21 @@ function filteredData (value, types, info) {
             wrap.style.display = "block"
         } else {
             wrap.style.display = "none"
-        }
+        };
     })
         
     
-}
+};
 
-// card for pokemon that are displayed on page load
-
-
-
- const searchBtn = document.querySelector('#search-btn')
- searchBtn.addEventListener('click', searchPokemon)   
+// setting 
+const searchBtn = document.querySelector('#search-btn')
+searchBtn.addEventListener('click', searchPokemon)   
 function searchPokemon (event) {
-    event.preventDefault()
-    const searchValue = document.querySelector('.form-control').value
-    showSinglePokemonInfo(searchValue) 
-    // console.log(searchValue)
+    event.preventDefault();
+    const searchValue = document.querySelector('.form-control').value;
+    showSinglePokemonInfo(searchValue);
+};
 
-}
 // setting up the show single pokemon info
 function showSinglePokemonInfo(pokemonName) {
   // template that will plug in the parameter pokemon name
@@ -164,7 +129,7 @@ function showSinglePokemonInfo(pokemonName) {
           "pokemon not found";
       } else {
         return response.json();
-      }
+      };
     })
     .then(function (data) {
       // console.log(data);
@@ -174,9 +139,10 @@ function showSinglePokemonInfo(pokemonName) {
       } else {
         document.querySelector("#fetched-pokemon").innerHTML =
           "pokemon not found";
-      }
+      };
     });
-}
+};
+
 //function setting up the display searched pokemon
 function displaySearchedPokemon(info) {
   // console.log(info)
@@ -199,21 +165,31 @@ function displaySearchedPokemon(info) {
     `;
 
   pokemonOutput.innerHTML = card;
-}
+};
 
 // pulling the values for the left and right buttons
 let leftArrow = document.querySelector("#left-arrow");
 let rightArrow = document.querySelector("#right-arrow");
-// adding the function for leftPagination
-function rightPagination() {
-  offset += 20;
-  var emptyDisplayList = [];
-  displayList(emptyDisplayList);
-  getAll151FetchCall(20);
-}
 
+// adding the function for rightPagination, if button is clicked, set fetch call offset to add in 20
+// and set the current HTML to be  blank, erasing all of the pokemon currently on the page
+function rightPagination() {
+  if (offset == 140){
+    return;
+  } else {
+    offset += 20;
+    var emptyDisplayList = [];
+    displayList(emptyDisplayList);
+    getAll151FetchCall(20);
+    console.log(offset);
+};
+  };
+  
+
+// adding the function for leftPagination, if button is clicked, set fetch call offset to subtract 20
+// and set the current HTML to be  blank, erasing all of the pokemon currently on the page
 function leftPagination() {
-  if (offset == 0 || offset == 131) {
+  if (offset == 0) {
     return;
   } else {
     offset -= 20;
@@ -222,7 +198,8 @@ function leftPagination() {
     getAll151FetchCall(20);
     console.log(offset);
   }
-}
+};
+
 // adding event listeners to the buttons
 leftArrow.addEventListener("click", leftPagination);
 rightArrow.addEventListener("click", rightPagination);
@@ -234,6 +211,4 @@ getAll151FetchCall(20);
 
 
 
- // on starting of the page, place in 20 pokemon to the call. 
- getAll151FetchCall(20)
 

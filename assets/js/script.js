27df42a3e683b2af93fafb3aedc64aa9a0b0fb92
waustin45 <108015666+waustin45@users.noreply.github.com>
2023-01-17 10:31:34 -0500

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,23 +78,25 @@ function displayList(items) {
   } else {
     const card = `
 
-      <div class="container-fluid pokemon-card parent">
-        <div id=${items[0].types[0].type.name} class="wrapper list-wrapper card shadow p-3 mb-5 bg-body-tertiary rounded ";>   
+      <div class="container-fluid pokemon-card  parent">
+        <div id=${items[0].types[0].type.name} class="wrapper list-wrapper card shadow  mb-5 bg-body-tertiary rounded ";>   
           <div class="info-wrapper ">
-          <h1 id="pokemon-name">${items[0].name} </h1>
+            <h1 id="pokemon-name">${items[0].name} </h1>
               <div class="stats-wrapper h25">
               <img id= "pokemon-img" class="w-100" src=${"./assets/pokemon/" + items[0].id + ".png"} alt=${items[0].name}/>
           </div>
             <div id=${items[0].name}  class="hover-wrapper">
-            <div class= "row">
-            <div class="barContianer">HP:<div class="hp-div skill info-card-div" style="width:${items[0].stats[0].base_stat}%">${items[0].stats[0].base_stat}</div></div>
-            <div class="barContianer">Attack: <div class="attack-div skill info-card-div" style="width:${items[0].stats[1].base_stat}%"> ${items[0].stats[1].base_stat}</div>
-            <div class="barContianer">Defense:<div class="defense-div skill info-card-div" style="width:${items[0].stats[2].base_stat}%"> ${items[0].stats[2].base_stat}</div>
-            <div class="barContianer">Special-Attack:<div class="specialatk-div skill info-card-div" style="width:${items[0].stats[3].base_stat}%"> ${items[0].stats[3].base_stat}</div>
-            <div class="barContianer">Special-Defense:<div class="specialdef-div skill info-card-div" style="width:${items[0].stats[4].base_stat}%"> ${items[0].stats[4].base_stat}</div>
-            <div class="barContianer">Speed: <div class="speed-div info-card-div skill" style="width:${items[0].stats[5].base_stat}%">${items[0].stats[5].base_stat}</div>
-          </div>
+              <div class= "row rando-row">
+                <div class="barContianer">HP:<div class="hp-div skill info-card-div" style="width:${items[0].stats[0].base_stat}%">${items[0].stats[0].base_stat}</div></div>
+                <div class="barContianer">Attack: <div class="attack-div skill info-card-div" style="width:${items[0].stats[1].base_stat}%"> ${items[0].stats[1].base_stat}</div></div>
+                <div class="barContianer">Defense:<div class="defense-div skill info-card-div" style="width:${items[0].stats[2].base_stat}%"> ${items[0].stats[2].base_stat}</div></div>
+                <div class="barContianer">Special-Attack:<div class="specialatk-div skill info-card-div" style="width:${items[0].stats[3].base_stat}%"> ${items[0].stats[3].base_stat}</div></div>
+                <div class="barContianer">Special-Defense:<div class="specialdef-div skill info-card-div" style="width:${items[0].stats[4].base_stat}%"> ${items[0].stats[4].base_stat}</div></div>
+                <div class="barContianer">Speed: <div class="speed-div info-card-div skill" style="width:${items[0].stats[5].base_stat}%">${items[0].stats[5].base_stat}</div></div>
+              </div>
+            </div>  
         </div>
+      </div>
      `
     listOutput.innerHTML += card
     }
@@ -271,15 +273,21 @@ function addToTeam(){
     // console.log(pokemonCard);
     listOutput.addEventListener("click", function(event){
        
-        console.log(event.currentTarget + " event works")
-        targetCardPhoto.push(event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+        console.log(event.target.innerHTML + " event works")
+        if (targetCardPhoto.length < 6) {
+          targetCardPhoto.push(event.target.parentElement.parentElement.parentElement.parentElement.parentElement);
+        } else {
+           console.log("can only have six pokemon to a team")
+           return
+        }
+        
         console.log(targetCardPhoto)
         var footerDisplay =document.querySelector(".ul-team");
         var footerimage = document.createElement("IMG");
         const mappy = targetCardPhoto.map(instance => {
           
-          return `<div class="mapped-div parent">${instance.innerHTML}</div>`
-        })
+          return `<div class="mapped-div parent container">${instance.innerHTML}</div>`
+        }).join('')
         footerDisplay.innerHTML = mappy
         removeTeam(mappy)
     });
@@ -297,6 +305,41 @@ function removeTeam (info) {
   })
   
 }
+
+const addTeamBtn = document.querySelector(".add-team-btn")
+addTeamBtn.addEventListener('click', saveTeamToStorage)
+function saveTeamToStorage () {
+  const dropDown = document.querySelector(".team-dropdown")
+  let dropValue = dropDown.innerHTML
+  var footerDisplay =document.querySelector(".ul-team");
+  localStorage.setItem(`${dropValue}`, JSON.stringify(footerDisplay.innerHTML))
+  footerDisplay.innerHTML = "Team Saved!"
+  targetCardPhoto = []
+  console.log(footerDisplay.innerHTML)
+}
+
+function getFromStorage () {
+  var footerDisplay = document.querySelector(".ul-team");
+  console.log(JSON.parse(localStorage.getItem("team")))
+  footerDisplay.innerHTML = JSON.parse(localStorage.getItem(`${dropValue}`))
+}
+
+
+function setTeamList () {
+  const dropDown = document.querySelector(".team-dropdown")
+  const setTeam = document.querySelectorAll('.team-setter')
+  setTeam.forEach(each => {
+    each.addEventListener('click', () => {
+      let value = each.innerHTML
+      console.log(value)
+      dropDown.innerHTML = value
+    })
+    
+  })
+  
+}
+setTeamList()
+
 
 
 
